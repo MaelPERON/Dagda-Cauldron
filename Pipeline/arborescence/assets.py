862e641d1e7e68a851f97d8603aa983bebf2972f
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-09-28 18:15:36 +0200

    update(assets.py) implement main function logic as well as constant variables

---
+++ Pipeline/arborescence/assets.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import os
+import json
+import datetime
+import re
+
+SCRIPT_FOLDER = Path(__file__).parent
+ASSET_TYPES = {
+	"Characters": ["char","chars","character"],
+	"Props": ["prop","props"],
+	"Sets": ["set","setdress"],
+	"Parts": ["part","parts"],
+	"Vehicles": ["veh","vehicle"],
+}
+CONFIG_PATH = None
+ROOT_PATH = None
+
+if __name__ == "__main__":
+	root = r"G:\Mon Drive\ENSI\01_E4\Exos\taste_of_guerilla" # first argument
+	config = r"D:\Documents\Github\Dagda-Cauldron\Pipeline\arborescence\assets.json" # second argument
+	ROOT_PATH = fetch_resource_path(os.path.expandvars("%projet%/assets/"))
+	CONFIG_PATH = fetch_resource_path(os.path.expandvars("./assets.json"))
+
+	if not CONFIG_PATH.exists():
+		raise FileNotFoundError(f"Configuration file not found: {CONFIG_PATH}")
+
+	if not CONFIG_PATH.is_file() or CONFIG_PATH.suffix != ".json":
+		raise ValueError(f"Invalid configuration file: {CONFIG_PATH}")
+
+	aborescence = json.load(open(CONFIG_PATH))
+
+	generate_asset_tree("char_hero_v1", aborescence)
