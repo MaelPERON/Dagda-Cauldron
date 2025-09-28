06a61dad69e78cffd465e56d960e30c87672303f
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-09-28 18:24:34 +0200

    feat(assets.py) add create_tree_file function for file creation and directory management

--- Pipeline/arborescence/assets.py
+++ Pipeline/arborescence/assets.py
@@ -23,6 +23,11 @@ def fetch_resource_path(resource: str | Path) -> Path:
 
 	return path
 
+def create_tree_file(path: Path, content: str = ""):
+	content = os.path.expandvars(content)
+	path.parent.mkdir(parents=True, exist_ok=True)
+	with open(path, "w", encoding="utf-8") as f:
+		f.write(content)
 if __name__ == "__main__":
 	root = r"G:\Mon Drive\ENSI\01_E4\Exos\taste_of_guerilla" # first argument
 	config = r"D:\Documents\Github\Dagda-Cauldron\Pipeline\arborescence\assets.json" # second argument
